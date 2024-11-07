7a79ea81cb2888926c037d90251d258ba93bfd00
Samson Hill <samsonhill@Samsons-MacBook-Air.local>
2024-11-06 21:41:17 -0800

    temperature

--- function.js
+++ function.js
@@ -1,17 +1,17 @@
 console.log("Function.js")
 
 
-function convertTemperature(celsius){
+function convertTemperature(){
+    let celsius = prompt ("Enter Temperature here");
     console.log(celsius*9/5 +32);
     return celsius * 9/5 + 32;
 }
 
-convertTemperature(10); //50
-convertTemperature(20); //68
+// convertTemperature(10); //50
+// convertTemperature(20); //68
 
-let total = 0;
-let tempName = prompt ("Enter Temperature here");
+// let total = 0;
 
-function addTemperatureinCelsius(){
-    let tempName= prompt("Temperature to Celsius");
-}
+// function addTemperatureinCelsius(){
+//     let tempName= prompt("Temperature to Celsius");
+// }
